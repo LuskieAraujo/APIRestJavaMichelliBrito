cc9e39b46754ab017807a08144638a0855ffb021
Lucas Araújo <araujo_depaula@outlook.com>
2023-05-24 00:19:27 -0300

    implementação do fluxo principal da vaga de estacionamento

---
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -0,0 +1,32 @@
+package com.api.parkingcontrol.controllers;
+
+import com.api.parkingcontrol.dtos.ParkingSpotDto;
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.services.ParkingSpotService;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping(value = "/parking-spot")
+public class ParkingSpotController {
+    final ParkingSpotService service;
+    public ParkingSpotController(ParkingSpotService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto dto){
+        var model = new ParkingSpotModel();
+        BeanUtils.copyProperties(dto, model);
+        model.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
+        
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.save(model));
+    }
+}
---
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -0,0 +1,17 @@
+package com.api.parkingcontrol.services;
+
+import com.api.parkingcontrol.models.ParkingSpotModel;
+import com.api.parkingcontrol.repositories.ParkingSpotRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ParkingSpotService {
+    @Autowired
+    ParkingSpotRepository parkingSpotRepository;
+    @Transactional
+    public ParkingSpotModel save(ParkingSpotModel model) {
+        return parkingSpotRepository.save(model);
+    }
+}
