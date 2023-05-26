2f289546c155a80f95040164386598f1262666ea
Lucas Araújo <araujo_depaula@outlook.com>
2023-05-26 16:13:24 -0300

    -

--- src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
+++ src/main/java/com/api/parkingcontrol/controllers/ParkingSpotController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -20,13 +21,28 @@ public class ParkingSpotController {
     public ParkingSpotController(ParkingSpotService service) {
         this.service = service;
     }
-
+    
     @PostMapping
     public ResponseEntity<Object> saveParkingSpot(@RequestBody @Valid ParkingSpotDto dto){
+        if(service.existsByLicensePlate(dto.getLicensePlateCar())) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: License Plate Car is already in use!");
+        }
+        if(service.existsByParkingSpotNumber(dto.getParkingSpotNumber())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: Parking Spot is already in use!");
+        }
+        if(service.existsByApartmentAndBlock(dto.getApartment(), dto.getBlock())){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("Conflito: Parking Spot already registered for this apartment/block!");
+        }
+        
         var model = new ParkingSpotModel();
         BeanUtils.copyProperties(dto, model);
         model.setRegistrationDate(LocalDateTime.now(ZoneId.of("UTC")));
         
         return ResponseEntity.status(HttpStatus.CREATED).body(service.save(model));
+    }
+    
+    @GetMapping
+    public ResponseEntity<List<ParkingSpotModel>> getAllParkingSpot(){
+        return ResponseEntity.status(HttpStatus.OK).body(service.findAll());
     }
 }
--- src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
+++ src/main/java/com/api/parkingcontrol/services/ParkingSpotService.java
@@ -6,12 +6,30 @@ import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class ParkingSpotService {
     @Autowired
-    ParkingSpotRepository parkingSpotRepository;
+    ParkingSpotRepository repo;
     @Transactional
     public ParkingSpotModel save(ParkingSpotModel model) {
-        return parkingSpotRepository.save(model);
+        return repo.save(model);
+    }
+    
+    public boolean existsByParkingSpotNumber(String parkingSpotNumber) {
+        return repo.existsByParkingSpotNumber(parkingSpotNumber);
+    }
+    
+    public boolean existsByLicensePlate(String licensePlateCar) {
+        return repo.existsByLicensePlateCar(licensePlateCar);
+    }
+    
+    public boolean existsByApartmentAndBlock(String apartment, String block) {
+        return repo.existsByApartmentAndBlock(apartment, block);
+    }
+    
+    public List<ParkingSpotModel> findAll() {
+        return repo.findAll();
     }
 }
